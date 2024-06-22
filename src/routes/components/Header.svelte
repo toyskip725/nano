d60a45a35164e0d1f1ab308637aaa23bcc9acb40
toyskip725 <toyskip725@gmail.com>
2024-06-22 22:45:24 +0900

    [feature] create SideNavigation/Header component

---
+++ src/routes/components/Header.svelte
@@ -0,0 +1,11 @@
+<p class="title"><span class="title-icon">＊</span>nano</p>
+
+<style>
+  .title {
+    color: #d6d7d8;
+    user-select: none;
+  }
+  .title-icon {
+    color: #e66077;
+  }
+</style>
---
+++ src/routes/components/SideNavigation.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+
+<div class="side-nav">
+</div>
+
+<style>
+  .side-nav {
+    min-width: 200px;
+    border-right: 1px solid #3e4e66;
+  }
+</style>
