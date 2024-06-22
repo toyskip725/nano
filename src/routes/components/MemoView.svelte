cb1dd0ebdf18a824f9c0b44ed2b0b02a570d827f
toyskip725 <toyskip725@gmail.com>
2024-06-22 22:44:22 +0900

    [feature] create MemoView component

---
+++ src/routes/components/MemoView.svelte
@@ -0,0 +1,66 @@
+<script>
+  export let content;
+
+  const onClickEdit = () => {
+    alert("edit clicked");
+  };
+
+  const onClickDelete = () => {
+    alert("delete clicked");
+  };
+</script>
+
+<div class="memo">
+  <p>{content}</p>
+  <div class="info-wrapper">
+    <span class="info">2024/06/xx 12:00</span>
+    <div class="info-button-wrapper">
+      <button type="button" class="info-button" on:click={onClickEdit}>
+        <span class="info-button-icon material-symbols-outlined">edit</span>
+      </button>
+      <button type="button" class="info-button" on:click={onClickDelete}>
+        <span class="info-button-icon material-symbols-outlined">delete</span>
+      </button>
+    </div>
+  </div>
+</div>
+
+<style>
+  p {
+    color: #d6d7d8;
+  }
+
+  .memo {
+    max-width: 560px;
+    min-width: 300px;
+    border-bottom: 1px solid #3e4e66;
+    padding: 1em;
+    margin-left: 2em;
+    margin-right: 2em;
+  }
+
+  .info-wrapper {
+    display: flex;
+  }
+  .info {
+    color: #5d7599;
+    font-size: smaller;
+  }
+  .info-button {
+    background-color: #1f2733;
+    border: none;
+  }
+  .info-button:hover {
+    cursor: pointer;
+  }
+  .info-button-icon {
+    color: #5d7599;
+    font-size: large;
+  }
+  .info-button-icon:hover {
+    color: #e66077;
+  }
+  .info-button-wrapper {
+    margin-left: auto;
+  }
+</style>
