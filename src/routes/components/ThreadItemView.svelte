963dd28116396a6ca2ea99deded5baaa3b6f6ffb
toyskip725 <toyskip725@gmail.com>
2024-06-28 14:34:04 +0900

    [feature] create ThreadItemView component

---
+++ src/routes/components/ThreadItemView.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  // props
+  export let threadName: string;
+  export let onSelect: (threadName: string) => void;
+
+  const onClick = () => {
+    onSelect(threadName);
+  };
+</script>
+
+<div>
+  <button type="button" class="thread-item" on:click={onClick}>{threadName}</button>
+</div>
+
+<style>
+  .thread-item {
+    width: 100%;
+    color: #d6d7d8;
+    background-color: #1f2733;
+    font-size: medium;
+    text-align: left;
+    border: none;
+    border-bottom: 1px solid #3e4e66;
+    padding: 1em;
+  }
+  .thread-item:hover {
+    cursor: pointer;
+  }
+</style>
